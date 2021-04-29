5bebb9703a2b89a8d757674b6eb592940fd702fe
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-04-29 16:11:26 +0400

    add loops.js

---
+++ loops.js
@@ -0,0 +1,37 @@
+//WHILE
+
+let i = 0;
+while (i < 3) {
+  // shows 0, then 1, then 2
+  console.log(i);
+  i++;
+}
+
+//DO...WHILE
+
+let g = 0;
+do {
+  console.log(g);
+  g++;
+} while (g < 3); //ktpi 0, 1, 2
+
+//FOR
+
+/*
+for (begin; condition; step) {
+  // ... loop body ...
+}
+*/
+
+for (let h = 0; h < 3; h++) {
+  //h=0-ic sksum a, stugum a, h<3, katarum a body-n(console.log(h)), heto nor h++ a anum
+  // shows 0, then 1, then 2
+  console.log(h);
+}
+
+//loopy karanq stop tanq
+//break;-ov
+// if (!value) break; // (*) ashxatum a if-i het
+//loopi mi bani vrov karanq trnenq
+//if (i % 2 == 0) continue; eli ashxatum a if-i het
+//continue;-ov
