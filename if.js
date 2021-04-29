72f67d81469a7829917f234b101fae00f87597d0
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-04-29 15:46:10 +0400

    add if.js

---
+++ if.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//let result = condition ? value1 : value2;
+
+let qash = 119;
+let ardyunq =
+  qash > 120 ? console.log("shat chax e") : console.log("shat chax che");
+
+//verevinn u nerqeviny nuynn en
+if (qash > 120) {
+  console.log("shat chax e");
+} else {
+  console.log("shat chax che");
+}
