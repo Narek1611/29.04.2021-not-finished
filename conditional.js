090a96e7da8d4b15177a1d65c83ae85e91362855
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-04-29 15:52:27 +0400

    renamed if.js to conditional.js and added some info about switch in it

--- conditional.js
+++ conditional.js
@@ -1,5 +1,4 @@
-"use strict";
-
+//IF, ?
 //let result = condition ? value1 : value2;
 
 let qash = 119;
@@ -11,4 +10,22 @@ if (qash > 120) {
   console.log("shat chax e");
 } else {
   console.log("shat chax che");
+}
+
+//SWITCH
+
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    console.log("Too small");
+    break;
+  case 4:
+    console.log("Exactly!");
+    break;
+  case 5:
+    console.log("Too big");
+    break;
+  default:
+    console.log("I don't know such values");
 }
