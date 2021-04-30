eb185055920f133792fa649aa2ac2daab5e98654
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-04-30 16:58:20 +0400

    add full#16 and right findSign.js

--- 16.js
+++ 16.js
@@ -1,7 +1,12 @@
-let single = -68;
+let single = 8;
 let numbers = [1, 4, 7, -68, 0, 25];
+let diff = [];
 for (i = 0; i < numbers.length; i++) {
   if (numbers[i] === single) {
-    console.log[numbers[i]];
+    console.log(i);
+  } else {
+    diff.push(Math.abs(numbers[i] - single));
   }
 }
+console.log(diff);
+console.log(diff.indexOf(Math.min(...diff)));
--- findSign.js
+++ findSign.js
@@ -1,25 +1,47 @@
-let a = 5;
-let b = -6;
-let c = -3;
+// let a = 5;
+// let b = -6;
+// let c = -3;
 
-if (a === 0 || b === 0 || c === 0) {
+// if (a === 0 || b === 0 || c === 0) {
+//   console.log("unsigned");
+// } else if (a > 0 && b > 0 && c > 0) {
+//   console.log("+");
+// } else if (a > 0 && b < 0 && c > 0) {
+//   console.log("-");
+// } else if (a > 0 && b > 0 && c < 0) {
+//   console.log("-");
+// } else if (a > 0 && b < 0 && c < 0) {
+//   console.log("+");
+// } else if (a > 0 && b < 0 && c < 0) {
+//   console.log("+");
+// } else if (a < 0 && b > 0 && c > 0) {
+//   console.log("-");
+// } else if (a < 0 && b < 0 && c > 0) {
+//   console.log("+");
+// } else if (a < 0 && b > 0 && c < 0) {
+//   console.log("+");
+// } else if (a < 0 && b < 0 && c < 0) {
+//   console.log("-");
+// }
+
+let arr = [-15, -6, 8];
+let countOfMinuses = 0;
+let isZero = false;
+for (let i = 0; i < arr.length; i++) {
+  const item = arr[i];
+
+  if (item === 0) {
+    isZero = true;
+  }
+
+  if (item < 0) {
+    countOfMinuses += 1;
+  }
+}
+if (isZero) {
   console.log("unsigned");
-} else if (a > 0 && b > 0 && c > 0) {
+} else if (countOfMinuses % 2 === 0) {
   console.log("+");
-} else if (a > 0 && b < 0 && c > 0) {
-  console.log("-");
-} else if (a > 0 && b > 0 && c < 0) {
-  console.log("-");
-} else if (a > 0 && b < 0 && c < 0) {
-  console.log("+");
-} else if (a > 0 && b < 0 && c < 0) {
-  console.log("+");
-} else if (a < 0 && b > 0 && c > 0) {
-  console.log("-");
-} else if (a < 0 && b < 0 && c > 0) {
-  console.log("+");
-} else if (a < 0 && b > 0 && c < 0) {
-  console.log("+");
-} else if (a < 0 && b < 0 && c < 0) {
+} else if (countOfMinuses % 2 !== 0) {
   console.log("-");
 }
