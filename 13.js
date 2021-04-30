88ae9bce54d418ae27e1350b39bf08075a36debc
NAREK KARAPETYAN <krptnrkrptnrk@gmail.com>
2021-04-30 10:18:56 +0400

    solved 13 and 15

--- 13.js
+++ 13.js
@@ -1,4 +1,4 @@
-let array = [7, 5, 4, "tox", undefined, true];
+let array = [7, 5, 4, 8, 15, 22, "tox", undefined, true];
 let arr2 = [];
 for (i = 0; i < array.length; i++) {
   if (typeof array[i] === typeof 1) {
@@ -8,3 +8,12 @@ for (i = 0; i < array.length; i++) {
 console.log(arr2);
 
 //kentutyunn u zuygutyuny heto kporcem
+
+let arr3 = [];
+for (i = 0; i < arr2.length; i++) {
+  if (arr2[i] % 2 === 0) {
+    arr3.push(arr2[i]);
+  } else {
+    arr3.unshift(arr2[i]);
+  }
+}
--- 15.js
+++ 15.js
@@ -1,7 +1,12 @@
 let arr = ["banan", "garoxik", "dzi", "armagedon"];
-let arrLength = [];
+let arrLengths = [];
 for (i = 0; i < arr.length; i++) {
-  arrLength.push(arr[i].length);
+  arrLengths.push(arr[i].length);
 }
-console.log(arrLength);
+console.log(arrLengths);
 //qanaknery lcrel em arrayi mej, uxxaki petq a nenc funkcia vor arrayic khani amenamec yev amenapoqr tvery u kgumari
+arrLengths.sort(function (a, b) {
+  return a - b;
+});
+console.log(arrLengths);
+console.log(arrLengths[0] + arrLengths[arrLengths.length - 1]);
